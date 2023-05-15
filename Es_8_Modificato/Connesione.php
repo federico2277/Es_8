3157aa96342b6e0751485eda25f99cfa625da220
federico2277 <101802997+federico2277@users.noreply.github.com>
2023-05-15 13:47:45 +0200

    Commit1

---
+++ Es_8_Modificato/Connesione.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?phP
+$host = "localhost";
+$root = "root";
+$password ="";
+$db = "db_es8";
+
+$connessione = mysqli_connect($host, $root, $password, $db);
+
+?>
+</body>
+</html>
